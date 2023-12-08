d05c718757ca0da56946d6da0858e837b6810fed
Janani Hemamali <66689414+janaweerakkody@users.noreply.github.com>
2023-12-08 18:46:16 +0530

    Initial Commit

---
+++ src/main/java/developerIQ/usercommitlogservice/UserCommitLogServiceApplication.java
@@ -0,0 +1,13 @@
+package developerIQ.usercommitlogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCommitLogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCommitLogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/developerIQ/usercommitlogservice/UserCommitLogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package developerIQ.usercommitlogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCommitLogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
